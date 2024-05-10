97d49c7d06720151ca9f25aa35fd711386e212b1
dexnis8 <dexnis30@gmail.com>
2024-05-10 00:50:12 -0700

    fixed render error

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,8 @@ function App() {
               
                 <h1>Layer</h1>
               </TransitionComponent>
-            } */}
-          />
+            } 
+          />*/}
         </Routes>
       </TransitionProvider>
     </>
